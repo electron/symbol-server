db675f917d7272af5be96b162270400de5c61901
Felix Rieseberg <felix@felixrieseberg.com>
2025-07-09 18:56:03 -0700

    chore: Add Claude (#18)

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const proxy = httpProxy.createProxyServer({
   changeOrigin: true,
 });
 
-const APPS_TO_ALIAS = ['slack', 'notion', 'notion dev'];
+const APPS_TO_ALIAS = ['slack', 'notion', 'notion dev', 'claude', 'claude nest'];
 
 // temporary hack to handle apps that rename Electron / Electron Helper --> My App / My App Helper
 // this should be removed once we have a proper solution for upstream crash
